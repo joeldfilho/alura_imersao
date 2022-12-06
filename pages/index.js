69f9154134e819db1174564da05bb50fdb8019b5
joeldfilho <joeldiasfilho@hotmail.com>
2022-12-06 07:55:17 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
 import config from "../config.json"
 import Header from "./components/header"
 function HomePage(){
-    const mensagem = "Bem vindo ao Alura Tube"
-    const header = Header
-    const fundoEstilo = { backgroundColor: "red" }
-    return <div style = {fundoEstilo}>
-        <Header></Header>
-    </div>
+    return <iframe title="Dashboard - Detalhamento" width="1920" height="1080" src="https://app.powerbi.com/view?r=eyJrIjoiZjM1NjgwZmMtOWIwNS00NDZiLWEzODEtMjc3NjIzOTBjOTc3IiwidCI6IjdlOTNlMjg2LWIyOWEtNDQ1NC1hNDFhLWU4NDE5ZWM5ZGViNSJ9" frameborder="0" allowFullScreen="true"></iframe>
 }
 
 export default HomePage
